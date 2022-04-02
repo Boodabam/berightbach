5e9886bdd832aecbf17de8ed1fa6c52cc31138d3
Boodabam <boodabam91@gmail.com>
2022-04-02 15:07:20 +0200

    changement fichier de lecture

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ Evidemment à changer si l'oscarito devient un hyperparamètre à optimiser
 
 def load_data(path=path):
     data = json.load(open(path, "r"))
-    X = np.array(data["features"])
+    X = np.array(data["mfcc"])
     Y = np.array(data["labels"])
-    nb_classes = data["nb_classes"]
+    nb_classes = len(data['mapping'])
     return X, Y, nb_classes
 
 def plot_acuracy(network):
